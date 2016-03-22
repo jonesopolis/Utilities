4b66c41ddfc0c9f00d18db05967ae40e69e17899
jonesopolis <davidarector@gmail.com>
2016-03-22 17:54:59 -0400

    real first

---
+++ Jones.Utilities/LiveConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Jones.Utilities
+{
+    public sealed class LiveConfig<T>
+    {
+        private readonly string _configPath;
+
+        public LiveConfig(string filePath)
+        {
+            if (filePath == null)
+            {
+                throw new ArgumentNullException(nameof(filePath));
+            }
+
+            _configPath = filePath;
+
+            if (!File.Exists(_configPath))
+            {
+                throw new FileNotFoundException(_configPath);
+            }
+        }
+
+        public T Configuration { get; private set; }
+    }
+}
