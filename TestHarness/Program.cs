67d5380a64f6642865c8bb537d2b546a2159ec9f
jonesopolis <davidarector@Gmail.com>
2016-03-24 23:19:50 -0400

    test harness

---
+++ TestHarness/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jones.Utilities;
+
+namespace TestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var live = new LiveConfig<IEnumerable<Config>>("config.json");
+            live.Changed += () => Console.WriteLine(string.Join(", ", live.Configuration.Select(c => c.Test)));
+            live.Unavailable += () => Console.WriteLine("Unavailable");
+            live.Watch();
+
+            Console.Read();
+        }
+    }
+
+    public class Config
+    {
+        public string Test { get; set; }
+    }
+}
